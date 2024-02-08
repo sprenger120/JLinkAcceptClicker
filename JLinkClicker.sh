6663627cf6a74f5fcce73a0cf00225b8a9e2acf7
Isidro Arias <isidro.arias@wslw.es>
2024-02-08 11:41:31 +0100

    --sync argument instead of polling

--- JLinkClicker.sh
+++ JLinkClicker.sh
@@ -2,14 +2,12 @@
 
 for (( ; ; ))
 do
-	WID=$(xdotool search --name "J-Link\sV\w\.?\w{1,4}\s\-\sTerms\sof\suse")
+	WID=$(xdotool search --sync --name "J-Link\sV\w\.?\w{1,4}\s\-\sTerms\sof\suse")
 	#echo $WID
 	if [ -n "$WID" ]; then
-		sleep 0.5
 		xdotool windowfocus $WID
 		xdotool key --window $WID "alt+a"
 		echo "found"
 		sleep 0.5
 	fi
-	sleep 0.1
 done
